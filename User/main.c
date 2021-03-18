048626ae9cfefe2bdb8313fd7fb087d569d7eea1
ouer1224 <ouer1224@qq.com>
2021-03-18 13:30:52 +0800

    clean

--- User/main.c
+++ User/main.c
@@ -3,64 +3,6 @@
 #include "uart3.h"
 #include "os_sprintf.h"
 
-#include "xtos.h"
-
-
-
-
-
-void delay(int c) 
-{
-    for (int i = 0; i < c; i++);
-}
-
-#define TASKA_STK_SIZE 1024
-#define TASKB_STK_SIZE 1024
-static uint32_t taskA_Stk[TASKA_STK_SIZE];
-static uint32_t taskB_Stk[TASKB_STK_SIZE];
-
-static struct xtos_task_struct taskA;
-static struct xtos_task_struct taskB;
-
-void task_switch() 
-{
-    if (gp_xtos_cur_task == &taskA)
-        gp_xtos_next_task = &taskB;
-    else
-        gp_xtos_next_task = &taskA;
-
-    xtos_context_switch();
-}
-
-void taska() {
-    while (1) {
-        //led_set_color(100, 0, 5);
-        
-		for (int i = 0; i < 1000; i++)
-		{
-            delay(10000);
-		}
-		tog_pin_port(LED3);
-		tog_pin_port(LED1);
-        task_switch();
-    }
-}
-
-void taskb() {
-    while (1) {
-        //led_set_color(5, 0, 100);
-        for (int i = 0; i < 1000; i++)
-		{
-            delay(10000);
-		}
-		tog_pin_port(LED4);
-		tog_pin_port(LED2);
-        task_switch();
-    }
-}
-
-
-
 
 
 
@@ -88,16 +30,11 @@ int main(void)
 	init_ins_res_port();
 	init_led_port();
 	
-	
-	
-    xtos_create_task(&taskA, taska, &taskA_Stk[TASKA_STK_SIZE - 1]);
-    xtos_create_task(&taskB, taskb, &taskB_Stk[TASKB_STK_SIZE - 1]);
+	tog_pin_port(LED1);
+	tog_pin_port(LED3);
 
-    gp_xtos_next_task = &taskA;
-
-    xtos_start();
-
-    while (1) {
+    while (1) 
+	{
 
     }
 	return 1;
