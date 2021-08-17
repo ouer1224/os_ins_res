19060459a5e7a3ece354f1ac9d7890be446b1198
ouer1224 <ouer1224@qq.com>
2021-08-17 09:52:22 +0800

    tmp:正在添加ad7699驱动的接口

---
+++ User/ad7699.h
@@ -0,0 +1,11 @@
+#ifndef __AD7699_H__
+#define __AD7699_H__
+
+#include "../os/os_typedef.h"
+
+fnDri uint8_t init_ad7699(void);
+fnDri uint8_t start_conver_ad7699(void);
+fnDri uint16_t LoopReadVal_7699(uint8_t id );
+
+
+#endif
---
+++ os/os_typedef.h
@@ -0,0 +1,13 @@
+#ifndef __OS_TYPE_H__
+#define __OS_TYPE_H__
+
+
+#define fnGlo	extern
+#define fnPrv	static
+
+#define fnDri	extern
+#define fnApp	extern
+
+
+#endif
+
